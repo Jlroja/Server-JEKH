5fd6d9f975deff349060006847ca117ea573280e
Javier Lasso <125415549+Jlroja@users.noreply.github.com>
2024-06-20 23:04:03 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const { Server } = require("socket.io");
+
+const clientUrl = "http://localhost:3000"; // Servidor Local
+const clientUrlDeploy = "https://proyecto-integrador-kohl-ten.vercel.app/"; // Servidor de despliegue
+
+
+const port = 5000;
+
+const io = new Server({
+    cors:{
+        origin: [clientUrl, clientUrlDeploy]
+    },
+});
+
+/**
+ * Start listening on the specified port.
+ */
+io.listen(port);
+
+let players = [];
+
+io.on('connection', (socket)=>{
+    
+    console.log(
+        'player joined with ID',
+        socket.id, ". There are " +
+        io.engine.clientsCount +
+        " players connected"
+    );
+
+    //Realiza la coneccion inicial de los jugadores
+    //Funciona
+    socket.on('player-connected', ()=>{
+        players.push({
+            id: socket.id,
+            urlAvatar: io.engine.clientsCount === 1 ?
+            "/assets/models/avatars/Cientific.glb":
+            "/assets/models/avatars/Engineer.glb",
+            position: null,
+            rotation: null,
+            animation: "Idle"
+        });
+        socket.emit('players-connected', players);
+    })
+
+    socket.on("moving-player", (valuesTranformPlayer) => {
+        const player = players.find( player => player.id === socket.id);
+        player.position = valuesTranformPlayer.position;
+        player.rotation = valuesTranformPlayer.rotation;
+        socket.broadcast.emit("updates-values-transform-player", player);
+    })
+
+    socket.on("change-animation", (animation) =>{
+        const player = players.find( player => player.id === socket.id);
+        player.animation = animation;
+        socket.broadcast.emit("update-animation", player);
+    })
+
+
+    socket.on('disconnect', ()=>{
+        console.log("Jugador desconectado");
+        players = players.filter(player =>player.id !== socket.id);
+        console.log(
+            "Player disconnected with ID",
+            socket.id, ". There are " +
+            io.engine.clientsCount +
+            " players connected"
+        );
+        socket.emit('players-connected', players);
+    })
+
+});
